03e29543a74dd879fb6a1ce03abb6e66e6f6bbf5
Geoffrey Gross <geoffreyrgross@gmail.com>
2016-05-23 18:01:49 -0500

    Load Foundation on every page transition.

--- static/js/transitions.js
+++ static/js/transitions.js
@@ -31,6 +31,7 @@ module.exports = {
             if(falling != null && $('#landing-container').length){
                     falling();
             }
+            $(document).foundation();
         });
     },
 
